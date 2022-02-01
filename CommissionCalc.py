a571c7de4333bb2632bdcec98561c6f37be8474d
Lemar Aman <59219805+LemarAman@users.noreply.github.com>
2022-02-01 14:51:25 -0800

    Remove Redundant Data

--- CommissionCalc.py
+++ CommissionCalc.py
@@ -1,13 +1,4 @@
-'''
-1-50 evals generated - $0.20 per eval
-51+ evals generated - $1 per eval
 
-1-15 evals turned signed client - $1 per client
-16+ evals turned signed client - $3 per client
-
-test nums: 46 evals generated $9.20, 20 signed ct $30.
-76 eval gen $36, 28 signed ct $54.
-'''
 import PySimpleGUI as sg
 import os
 
@@ -22,10 +13,9 @@ event, values = window.read()
 
 window.close()
 
-# evalsGenerated = int(input("How many evaluations generated? "))
 evalsGenerated = int(values[0])
 signedClients = int(values[1])
-# signedClients = int(input("How many evaluations turned signed client? "))
+
 
 totalEvalCommission = 0
 totalSignedCommission = 0
